93d9a7d6141c118fe151d7bac7d08d607ae36a41
baba_s <shotababa1206@gmail.com>
2020-06-20 13:04:45 +0900

    first commit

---
+++ Scripts/CameraUsedOnGlobalCanvas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// グローバルな Canvas で使用するカメラを検知するコンポーネントのインターフェイス
+	/// </summary>
+	public interface ICameraUsedOnGlobalCanvas
+	{
+		//================================================================================
+		// プロパティ
+		//================================================================================
+		/// <summary>
+		/// グローバルな Canvas で使用するカメラを返します
+		/// </summary>
+		Camera Camera { get; }
+	}
+
+	/// <summary>
+	/// グローバルな Canvas で使用するカメラを検知するコンポーネント
+	/// </summary>
+	[DisallowMultipleComponent]
+	[RequireComponent( typeof( Camera ) )]
+	public sealed class CameraUsedOnGlobalCanvas :
+		MonoBehaviour,
+		ICameraUsedOnGlobalCanvas
+	{
+		//================================================================================
+		// 変数
+		//================================================================================
+		private Camera m_camera;
+
+		//================================================================================
+		// 変数（static readonly）
+		//================================================================================
+		private static readonly List<CameraUsedOnGlobalCanvas> ms_list = new List<CameraUsedOnGlobalCanvas>();
+
+		//================================================================================
+		// プロパティ
+		//================================================================================
+		public Camera Camera => m_camera;
+
+		//================================================================================
+		// プロパティ（static）
+		//================================================================================
+		public static IReadOnlyList<ICameraUsedOnGlobalCanvas> List         => ms_list;
+		public static ICameraUsedOnGlobalCanvas                ActiveCamera => ms_list[ ms_list.Count - 1 ];
+
+		//================================================================================
+		// イベント（static）
+		//================================================================================
+		public static event Action<ICameraUsedOnGlobalCanvas> OnChange;
+
+		//================================================================================
+		// 関数
+		//================================================================================
+		/// <summary>
+		/// 有効になった時に呼び出されます
+		/// </summary>
+		private void OnEnable()
+		{
+			ms_list.Add( this );
+
+			if ( m_camera == null )
+			{
+				m_camera = GetComponent<Camera>();
+			}
+
+			OnChange?.Invoke( this );
+		}
+
+		/// <summary>
+		/// 無効になった時に呼び出されます
+		/// </summary>
+		private void OnDisable()
+		{
+			ms_list.Remove( this );
+		}
+	}
+}
---
+++ Scripts/SetCameraOnGlobalCanvas.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// グローバルな Canvas で使用するカメラを設定するコンポーネント
+	/// </summary>
+	[DisallowMultipleComponent]
+	[RequireComponent( typeof( Canvas ) )]
+	internal sealed class SetCameraOnGlobalCanvas : MonoBehaviour
+	{
+		//==============================================================================
+		// 変数
+		//==============================================================================
+		private Canvas m_canvas;
+
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// 初期化する時に呼び出されます
+		/// </summary>
+		private void Awake()
+		{
+			m_canvas = GetComponent<Canvas>();
+
+			CameraUsedOnGlobalCanvas.OnChange += OnChangeCameraUsedOnGlobalCanvas;
+		}
+		
+		/// <summary>
+		/// 開始する時に呼び出されます
+		/// </summary>
+		private void Start()
+		{
+			OnChangeCameraUsedOnGlobalCanvas( CameraUsedOnGlobalCanvas.ActiveCamera );
+		}
+
+		/// <summary>
+		/// 破棄する時に呼び出されます
+		/// </summary>
+		private void OnDestroy()
+		{
+			CameraUsedOnGlobalCanvas.OnChange -= OnChangeCameraUsedOnGlobalCanvas;
+		}
+
+		/// <summary>
+		/// グローバルな Canvas で使用するカメラが変更された時に呼び出されます
+		/// </summary>
+		private void OnChangeCameraUsedOnGlobalCanvas( ICameraUsedOnGlobalCanvas worldCamera )
+		{
+			m_canvas.worldCamera = worldCamera.Camera;
+		}
+	}
+}
